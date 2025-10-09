c90ccd4dbdb601b92a7a5570afd316a1fc8376b1
dynii <dyniiyeyo@naver.com>
2025-10-09 21:27:41 +0900

    251009 NOLTicket Crawling

--- src/JavaAPI/Jsoup/jsoupEx.java
+++ src/JavaAPI/Jsoup/jsoupEx.java
@@ -9,6 +9,7 @@ public class jsoupEx {
     public static void main(String[] args) {
         // NOL티켓으로 변경~
         String url = "http://ticket.interpark.com/TPGoodsList.asp?Ca=Liv";
+        String rtnUrl = "http://ticket.interpark.com";
         Document doc = null;
         try {
             doc = Jsoup.connect(url).get();
@@ -19,9 +20,17 @@ public class jsoupEx {
         String title1 = performances.select("caption").text();
         System.out.println(title1);
 
+        int count = 1;
         for (Element perf : performances.select("tr")) {
-            System.out.println(perf.select("td > span > a").text());
+            System.out.println(count + ". " + perf.select("td > span > a").text());
+            String perfPlace = perf.select("td.Rkdate > a").text();
+            System.out.println("장소 : " + perfPlace);
+            System.out.println("기간 : " +  perf.select("td.Rkdate").text().substring(perfPlace.length()));
+            System.out.println("예매링크 : " + rtnUrl + perf.select("td > span > a").attr("href"));
+            count++;
         }
+
+
 
     }
 }
